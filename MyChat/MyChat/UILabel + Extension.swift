c513f87507d613b0744236d7cef856dba0af0606
Ivan Nezgovorov <87833835+IvanNez@users.noreply.github.com>
2024-04-26 17:51:41 +0500

    added extension button, label, image, color, font

---
+++ MyChat/MyChat/UILabel + Extension.swift
@@ -0,0 +1,17 @@
+//
+//  UILabel + Extension.swift
+//  MyChat
+//
+//  Created by Иван Незговоров on 26.04.2024.
+//
+
+import Foundation
+import UIKit
+
+extension UILabel {
+    convenience init(text: String) {
+        self.init()
+        
+        self.text = text
+    }
+}
