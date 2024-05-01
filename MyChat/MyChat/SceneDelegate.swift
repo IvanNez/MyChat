4ab9451d64a7fe3d7ecb8ed403cc1dad69b811c1
Ivan Nezgovorov <87833835+IvanNez@users.noreply.github.com>
2024-05-01 18:14:08 +0500

    connect fire base

--- MyChat/MyChat/SceneDelegate.swift
+++ MyChat/MyChat/SceneDelegate.swift
@@ -41,7 +41,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func downloadScene(windowScene: UIWindowScene) {
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = ChatRequestViewController()
+        window.rootViewController = MainTabBarController()
         self.window = window
         window.makeKeyAndVisible()
     }
